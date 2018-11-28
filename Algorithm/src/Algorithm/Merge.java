9614930b0803d049289c1ba6c8749bc47df423c3
kimdonwon <ehddnjs0728@naver.com>
2018-11-28 15:56:17 +0900

    Merge Sort

---
+++ Algorithm/src/Algorithm/Merge.java
@@ -0,0 +1,71 @@
+package Algorithm;
+
+public class Merge {
+	static int n = 8;
+	static int sorted[];
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		sorted = new int [n];
+		int data[] = {7,6,5,8,3,5,9,1};
+		mergeSort(data,0,n-1);
+		for(int i=0;i<n;i++) {
+			System.out.println(data[i]);
+		}
+		
+		
+	}
+	
+	//m 시작점, middle 중간점, n 끝점
+	static void merge(int a[], int m,int middle, int n) {
+		//비교할 첫번째 배열에 첫번째 값
+		int i = m;
+		//비교할 두번째 배열에 두번째 값
+		int j = middle + 1;
+		//비교값을 넣을 배열의 첫번째 값
+		int k = m;
+		
+		// 작은 순서대로 배열에 삽입
+		while( i <= middle && j <= n) {
+			if(a[i] <= a[j]) {
+				sorted[k] = a[i];
+				i++;
+			}
+			else {
+				sorted[k] = a[j];
+				j++;
+			}
+			k++;
+		}
+		
+		//남은 데이터 삽입
+		if (i>middle) {
+			for(int t=j;t<=n;t++) {
+				sorted[k] = a[t];
+				k++;
+			}
+		}
+		else {
+			for(int t=i;t<=middle;t++) {
+				sorted[k] = a[t];
+				k++;
+			}
+		}
+		
+		//정렬된 배열을 삽입
+		for(int t = m; t <= n ; t++) {
+			a[t] = sorted[t];
+		}
+	}
+	
+	static void mergeSort(int a[],int m, int n) {
+		//크기가 1보다 큰 경우
+		if(m<n) {
+			int middle = (m+n)/2;
+			mergeSort(a,m,middle);
+			mergeSort(a,middle+1,n);
+			merge(a,m,middle,n);
+		}
+	}
+
+}
